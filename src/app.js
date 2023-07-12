c281510ec3e99ad240804733dd997eee18b17709
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-07-11 22:07:42 -0300

    Created connectToDatabase

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+import { connectToDatabase } from "./database/db.js";
+import { configDotenv } from "dotenv";
+
+/* API configuration*/
+dotenv.config();
+connectToDatabase();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+/* Entrypoints (rotas) */
+// app.use();
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta: ${PORT}`));
