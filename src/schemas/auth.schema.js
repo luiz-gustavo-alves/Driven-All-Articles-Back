5766e79751d65c1a8b7fef607f4855be7b07905d
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-07-12 12:55:46 -0300

    fix: Added image to Auth Schema

--- src/schemas/auth.schema.js
+++ src/schemas/auth.schema.js
@@ -5,7 +5,8 @@ const signUpSchema = joi.object({
     name: joi.string().max(64).required(),
     email: joi.string().max(256).required(),
     password: joi.string().min(3).max(256).required(),
-    confirmPassword: joi.string().min(3).max(256).required()
+    confirmPassword: joi.string().min(3).max(256).required(),
+    image: joi.string().max(4096).required()
 });
 
 const signInSchema = joi.object({
