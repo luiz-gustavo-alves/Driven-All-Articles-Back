cce9079ae57a86fcf287ca7c1a706be76d37f354
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-07-14 18:28:19 -0300

    feat: Get and Delete shopping cart products
    feat deatils:
    
    (*) validateProductID middleware

--- src/controllers/shoppingCart.controller.js
+++ src/controllers/shoppingCart.controller.js
@@ -1,29 +1,66 @@
 import db from "../database/db.js";
-import { ObjectId } from "mongodb";
+
+export const getProducts = async (req, res) => {
+
+    const { userID } = res.locals.session;
+
+    try {
+
+        const shoppingCart = await db.collection("shoppingCart").find({ userID }).toArray();
+        res.send(shoppingCart);
+
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
 
 export const addProduct = async (req, res) => {
 
-    let productID = req.params;
-    productID = new ObjectId(productID);
+    const { userID } = res.locals.session;
+    const { product, shoppingCart, productID } = res.locals;
+
+    if (shoppingCart) {
+        return res.status(409).send("Este produto já está no carrinho de compras!");
+    }
+
+    if (product.quantity <= 0) {
+        return res.status(401).send("Este produto não está mais disponível!");
+    }
 
     try {
-        const product = await db.collection("products").findOne({ _id: productID });
-        const shoppingCart = await db.collection("shoppingCart").findOne({ _id: productID });
-
-        if (shoppingCart) {
-            return res.status(409).send("Este produto já está no carrinho de compras!");
-        }
-
-        if (product.quantity <= 0) {
-            return res.status(401).send("Este produto não está mais disponível!");
-        }
 
         await db.collection("products").updateOne({ _id: productID }, { $inc: { quantity: -1} });
 
-        delete product.quantity
-        await db.collection("shoppingCart").insertOne({...product});
+        delete product._id;
+        delete product.userID;
+        delete product.quantity;
 
-        res.send({...product});
+        const newProductData = { ...product, productID, userID };
+
+        await db.collection("shoppingCart").insertOne({...newProductData});
+        delete newProductData.userID;
+
+        res.send({...newProductData});
+
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
+export const deleteProduct = async (req, res) => {
+
+    const { shoppingCart, productID } = res.locals;
+
+    if (!shoppingCart) {
+        return res.status(403).send("Este produto não está no carrinho de compras!");
+    }
+
+    try {
+
+        await db.collection("products").updateOne({ _id: productID }, { $inc: { quantity: 1} });
+        await db.collection("shoppingCart").deleteOne({ userID: shoppingCart.userID, productID });
+
+        res.sendStatus(200);
 
     } catch (err) {
         res.status(500).send(err.message);
