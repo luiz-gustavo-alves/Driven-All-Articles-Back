4e00e59c41e211a0dbcf632fc3bcd0acfcc9e541
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-07-14 14:55:02 -0300

    feat: List products via limit query param

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -6,6 +6,7 @@ export const createProduct = async (req, res) => {
     const { userID } = res.locals.session;
 
     try {
+
         const newProduct = {
 
             ...res.locals.data,
@@ -14,8 +15,42 @@ export const createProduct = async (req, res) => {
         }
 
         await db.collection("products").insertOne(newProduct);
-        res.send(newProduct);
+        res.sendStatus(201);
 
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
+export const getProducts = async (req, res) => {
+
+    const { limit } = req.query;
+    const numLimit = Number(limit);
+
+    if (limit !== undefined && (numLimit <= 0 || isNaN(numLimit))) {
+        return res.sendStatus(422)
+    }
+
+    try {
+
+        const offset = 20;
+        let nextOffset;
+
+        if (limit) {
+
+            const totalProducts = await db.collection("products").countDocuments();
+            const nextLimit = (numLimit + 1) * offset;
+            nextOffset = (nextLimit >= totalProducts) ? 0 : nextLimit;
+        }
+
+        const products = await db.collection("products")
+            .find()
+            .sort({ $natural: -1 })
+            .limit(limit === undefined ? offset : nextOffset)
+            .toArray()
+
+        res.send(products);
+        
     } catch (err) {
         res.status(500).send(err.message);
     }
