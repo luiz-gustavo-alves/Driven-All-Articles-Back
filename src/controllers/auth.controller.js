808d1256857b741a05eb2998b8db330e5c760c79
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-07-16 17:14:40 -0300

    fix: revert image in auth.controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,22 +5,9 @@
 import bcrypt from "bcrypt";
 import db from "../database/db.js";
 import { v4 as uuid } from "uuid";
-import multer from "multer";
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "uploads/");
-    },
-    filename: function (req, file, cb) {
-        const filename = `${uuid()}-${file.originalname}`;
-        cb(null, filename);
-    }
-});
-
-const upload = multer({ storage: storage });
 
 export async function register(req, res) {
-    const { name, email, password } = req.body;
+    const { name, email, password, image } = req.body;
 
     try {
         const user = await db.collection("users").findOne({ email });
@@ -29,25 +16,10 @@ export async function register(req, res) {
         }
 
         const cryptedPassword = bcrypt.hashSync(password, 10);
+        await db.collection("users").insertOne({ name, email, password: cryptedPassword, image });
 
-        upload.single("image")(req, res, function (err) {
-            if (err instanceof multer.MulterError) {
-                return res.status(500).send("Erro durante o upload da imagem.");
-            } else if (err) {
-                return res.status(500).send("Ocorreu um erro no servidor.");
-            }
-
-            const image = req.file ? req.file.filename : '';
-
-            db.collection("users").insertOne({
-                name,
-                email,
-                password: cryptedPassword,
-                image
-            });
-
-            res.sendStatus(201);
-        });
+        res.sendStatus(201);
+        
     } catch (err) {
         res.status(500).send(err.message);
     }
@@ -64,7 +36,7 @@ export async function login(req, res) {
         const user = await db.collection("users").findOne({ email });
         // se o usuario fornecido nao estiver no sevidor - vericiar se a senha esta correta
         if (!user || !bcrypt.compareSync(password, user.password)) {
-            return res.status(404).send({ message: "E-mail ou senha incorretos!" })
+            return res.status(404).send("E-mail ou senha incorretos!");
         };
 
         await db.collection("sessions").findOneAndDelete({ userID: user._id });
