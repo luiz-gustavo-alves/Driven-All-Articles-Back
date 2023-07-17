65665a1f8b340fe47c13fef99c805d9db52e3236
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-07-17 11:40:00 -0300

    feat: Added productCount to userInfo

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,9 +8,10 @@ export const getUserById = async (req, res) => {
     try {
 
         const products = await db.collection("products").findOne({ userID: new ObjectId(id) });
+        const productsList = await db.collection("products").find({ userID: new ObjectId(id) }).toArray();
         const user = await db.collection("users").findOne({ _id: products.userID });
 
-        res.send({ name: user.name, image: user.image });
+        res.send({ name: user.name, image: user.image, productsCount: productsList.length });
 
     } catch (err) {
         res.status(500).send(err.message);
